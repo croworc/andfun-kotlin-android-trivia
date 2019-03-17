4c41b8a14d055633beac01767580f1c1132b04c2
ikrotzky <ikr@croworc.com>
2019-03-17 16:34:56 +0100

    [Step06-Ex] feat: Add "About" menu in start screen

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,36 +17,48 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false)
+
         binding.playButton.setOnClickListener (
                 Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
-        // TODO (02) Create the new menu resource
+
+        // COMPLETED (02) Create the new menu resource
         // Right click on the res folder within the Android project and select New Resource File
         // Name it overflow_menu with resource type menu.  Add an About menu item with the ID of
         // the aboutFragment.
-        // TODO (03) Call setHasOptionsMenu(true)
+
+        // COMPLETED (03) Call setHasOptionsMenu(true)
         // This tells Android that our fragment has an Options Menu, so it will call
         // onCreateOptionsMenu
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
-    // TODO (04) Override onCreateOptionsMenu
+    // COMPLETED (04) Override onCreateOptionsMenu
     // Use the passed-in MenuInflater to inflate the overflow_menu
-
-    // TODO (05) Override onOptionsItemSelected
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+    // COMPLETED (05) Override onOptionsItemSelected
     // Return true if NavigationUI.onNavDestinationSelected returns true, else return
     // super.onOptionsItemSelected.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController())
+                ||  super.onOptionsItemSelected(item)
+    }
 }
