367f70bc3891a0d54185c5bb47be587af640fa8d
ikrotzky <ikr@croworc.com>
2019-03-18 23:02:05 +0100

    [Step08-Ex] feat: Add sharing w/ an intent

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,51 +16,70 @@
 
 package com.example.android.navigation
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
-import androidx.databinding.DataBindingUtil
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.*
 import androidx.core.app.ShareCompat
-import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-import android.content.pm.ResolveInfo
-import android.content.pm.PackageManager
-
-
 
 class GameWonFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+
+        // Set an OnClickListener which will return us to the GameFragment for another match
         binding.nextMatchButton.setOnClickListener { view: View ->
             view.findNavController().navigate(
                     GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        var args = GameWonFragmentArgs.fromBundle(arguments)
-        Toast.makeText(context,
-                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
-                Toast.LENGTH_LONG).show()
-        // TODO (01) Add setHasOptionsMenu(true)
+
+        // COMPLETED (01) Add setHasOptionsMenu(true)
         // This allows onCreateOptionsMenu to be called
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    // COMPLETED (02) Create getShareIntent method
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
 
-    // TODO (02) Create getShareIntent method
-    // TODO (03) Create shareSuccess method
-    // TODO (04) Override and fill out onCreateOptionsMenu
-    // Inflate the winner_menu and set the share menu item to invisible if the activity doesn't
-    // resolve
-    // TODO (05) Override onOptionsItemSelected
-    // Call the shareSuccess method when the item id matches R.id.share
+        return ShareCompat.IntentBuilder.from(activity)
+                .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+                .setType("text/plain")
+                .intent
+    }
+
+    // COMPLETED (03) Create shareSuccess method
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    // COMPLETED (04) Override and fill out onCreateOptionsMenu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        // Inflate the winner_menu ...
+        inflater.inflate(R.menu.winner_menu, menu)
+        // Check if the activity resolves...
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // ...and set the share menu item to invisible if the activity doesn't
+            // resolve
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    // COMPLETED (05) Override onOptionsItemSelected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Call the shareSuccess method when the item id matches R.id.share
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
